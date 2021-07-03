2ae2b58262496a49dd3149d826cf73f4e3414a09
ksw95 <73837999+ksw95@users.noreply.github.com>
2021-07-03 22:05:21 +0800

    Created Country.go file

---
+++ RESTapi/Models/Country.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"strings"
+
+	"github.com/microcosm-cc/bluemonday"
+)
+
+type Country struct {
+	Country string
+}
+
+func (country *Country) Sanitize() {
+	p := bluemonday.StrictPolicy
+	country.Country = strings.Title(strings.ToLower(p.Sanitize(country.Country)))
+}
