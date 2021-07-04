1ffcee8bffd7bf5dfdcfccf1aee77d8dd2573b82
ksw95 <kshaowei95@gmail.com>
2021-07-04 20:39:33 +0800

    Added responses functions

---
+++ RESTapi/Responses/Response.go
@@ -0,0 +1,32 @@
+package Responses
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func BasicRes(res http.ResponseWriter, statusCode int, data interface{}) {
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(statusCode)
+	err := json.NewEncoder(res).Encode(data)
+	if err != nil {
+		fmt.Fprintf(res, "%s", err.Error())
+	}
+}
+
+func ErrRes(res http.ResponseWriter, statusCode int, err error) {
+	if err != nil {
+		BasicRes(res, statusCode, struct {
+			Error string `json:"error"`
+		}{
+			Error: err.Error(),
+		})
+		return
+	}
+	BasicRes(res, http.StatusBadRequest, nil)
+}
